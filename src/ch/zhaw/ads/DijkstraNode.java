6bae96a61d092e8ab7dc73519ba59b71dd220a73
Takashi Yoshi <takashi@yoshi.email>
2016-10-31 21:22:17 +0100

    Added classes for Praktikum 7

---
+++ src/ch/zhaw/ads/DijkstraNode.java
@@ -0,0 +1,33 @@
+package ch.zhaw.ads;
+
+class DijkstraNode<E> extends Node<E> implements Comparable<DijkstraNode> {
+	boolean mark;
+	DijkstraNode<E> prev;
+	double dist;
+
+	public double getDist() {
+		return dist;
+	}
+
+	public void setDist(double dist) {
+		this.dist = dist;
+	}
+
+	public void setMark(boolean m) {
+		mark = m;
+	}
+
+	public boolean getMark() {
+		return mark;
+	}
+
+	public void setPrev(DijkstraNode<E> p) {
+		prev = p;
+	}
+
+	public DijkstraNode<E> getPrev() {
+		return prev;
+	}
+
+	public abstract int compareTo(DijkstraNode n);
+}
