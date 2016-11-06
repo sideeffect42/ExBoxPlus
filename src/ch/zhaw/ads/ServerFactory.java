709730e26b0e2a4bfad11209b0c5e0eb963d3bc1
Takashi Yoshi <takashi@yoshi.email>
2016-11-06 11:13:00 +0100

    ServerFactory: Clean up and improvements
    - Extracted package name guessing procedure into its own method
    - Added JavaDoc comments to public methods
    - Added a bit of logging

--- src/ch/zhaw/ads/ServerFactory.java
+++ src/ch/zhaw/ads/ServerFactory.java
@@ -9,52 +9,139 @@
 
 package ch.zhaw.ads;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.io.File;
+import java.util.List;
 
 public class ServerFactory {
 
-	public CommandExecutor createServer(String classBinaryName) throws Exception {
+	/**
+	 * Create CommandExecutor object from binary class name.
+	 * @param classBinaryName binary name of the class to load
+	 * @returns CommandExecutor object on success, null if class specified
+	 *          by classBinaryName is not a CommandExecutor subclass.
+	 * @throws ClassNotFoundException if classBinaryName is not in classpath
+	 * @throws InstantiationException if an exception is thrown inside of
+	 *         the constructor.
+	 */
+	public CommandExecutor createServer(String classBinaryName)
+		throws ClassNotFoundException, InstantiationException {
 		Class<?> clazz = Class.forName(classBinaryName);
 		if (clazz != null) {
-			return (CommandExecutor)clazz.newInstance();
+			Object inst;
+			try {
+				inst = clazz.newInstance();
+			} catch (Exception e) {
+				throw new InstantiationException("Could not instantiate class");
+			}
+
+			if ((inst instanceof CommandExecutor)) {
+				return (CommandExecutor)inst;
+			} else {
+				return null;
+			}
 		} else {
-			throw new ClassNotFoundException(("There is no class with binary name '" + classBinaryName + "' in the class path."));
+			String errorMessage = ("There is no class with binary name '"
+								   + classBinaryName + "' in the class path.");
+			throw new ClassNotFoundException(errorMessage);
 		}
 	}
 
-	public CommandExecutor createServer(String directory, String name) throws Exception {
+
+	/**
+	 * Create CommandExecutor object from a path to a classfile.
+	 * @param directory directory in which the classfile resides
+	 * @param name filename of the classfile
+	 * @returns CommandExecutor object on success, null if the class is
+	 *          not in the classpath.
+	 * @throws InstantiationException if an exception is thrown inside of
+	 *         the constructor.
+	 */
+	public CommandExecutor createServer(String directory, String name)
+		throws InstantiationException {
+		final String defaultPackageName = "ch.zhaw.ads";
 		String className = name.substring(0, name.indexOf('.'));
-		directory = directory.replaceAll("/+", "/");
 
-		ArrayList<String> pathComponents = new ArrayList<String>(Arrays.asList(directory.toString().split(File.separator)));
-
-		String packageName = "";
 		boolean foundClass = false;
 		CommandExecutor server = null;
 
 		try {
-			server = this.createServer(("ch.zhaw.ads." + className));
+			String classBinaryName = classBinaryNameOf(defaultPackageName,
+													   className);
+
+			server = this.createServer(classBinaryName);
 			foundClass = true;
 		} catch (ClassNotFoundException e1) {
-			while (!foundClass && pathComponents.size() > 0) {
+			System.err.printf("Loading with default package name %s failed%n",
+							  defaultPackageName);
+
+			String packageName = guessPackageName(directory, className);
+
+			if (packageName != null) {
+				System.err.printf("Guessed package name %s%n", packageName);
+				String classBinaryName = classBinaryNameOf(packageName,
+														   className);
+
 				try {
-					String newPrefix = pathComponents.remove((pathComponents.size() - 1));
-
-					if (newPrefix == null || newPrefix.length() < 1) { continue; }
-
-					packageName = (newPrefix + "." + packageName);
-
-					String classBinaryName = (packageName + className);
 					server = this.createServer(classBinaryName);
 					foundClass = true;
-				} catch (ClassNotFoundException e) {
-					// ignore
-				}
+				} catch (ClassNotFoundException e2) {}
+			} else {
+				System.err.println("Failed to guess package name. " +
+								   "Is the classfile in the classpath?");
 			}
 		}
 
 		return (foundClass ? server : null);
+	}
+
+	private static String classBinaryNameOf(
+		String packageName, String className) throws IllegalArgumentException {
+		if (className == null || className.length() < 1) {
+			throw new IllegalArgumentException("No className given");
+		}
+
+		if (packageName == null || packageName.length() < 1) {
+			return className;
+		}
+
+		return (packageName + "." + className);
+	}
+
+	private String guessPackageName(String directory, String className) {
+		String[] dirParts = directory.split(File.separator);
+		List<String> pcList = Arrays.asList(dirParts);
+		List<String> pathComponents = new ArrayList<String>(pcList);
+
+		boolean foundClass = false;
+		directory = directory.replaceAll("/+", "/");
+
+		String packageName = "";
+
+		while (!foundClass && pathComponents.size() > 0) {
+			int lastPCIndex = (pathComponents.size() - 1);
+			String newPrefix = pathComponents.remove(lastPCIndex);
+
+			if (newPrefix == null || newPrefix.length() < 1) { continue; }
+
+			packageName = (packageName.length() > 0
+						   ? (newPrefix + "." + packageName)
+						   : newPrefix);
+			String classBinaryName = classBinaryNameOf(packageName, className);
+
+			try {
+				CommandExecutor server = this.createServer(classBinaryName);
+				if ((server instanceof CommandExecutor)) {
+					foundClass = true;
+				}
+			} catch (ClassNotFoundException cnfe) {
+			} catch (InstantiationException ie) {
+				foundClass = true;
+			}
+		}
+
+		return (foundClass ? packageName : null);
 	}
 }
